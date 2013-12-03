b5bbd2728d4f5e4d6c60b44a9d0a79aed46e51b9
sujith <sujithonline8@gmail.com>
2013-12-03 16:44:44 +0530

    Copied vcard template to public using rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SujithVcard::Application.config.secret_token = '0e019c33eeead15450061e14d8ace099d501c097fec82e03a5f84aaa7a6da9e912145c05d9a2664f0cd8d7ccb5e639f5dff9b8c67b5e457a3037dcd2d79f91f7'
